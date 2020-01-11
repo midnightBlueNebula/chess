74e94d2555e70afafb85ae8485e1150d54adac9e
Onur Kaya <onurkaya8@outlook.com>
2020-01-11 23:14:46 +0300

    working on save/load mechanics

--- lib/chess.rb
+++ lib/chess.rb
@@ -3,34 +3,119 @@ class Chess
     attr_accessor :board, :game_status, :input, :last_casualty, :player_turn 
 
     def initialize
-        @board = Array.new(8) { |r| Array.new(8) { |c| "" } }
-        @board[0][0] = ["player_1","rook"]
-        @board[0][1] = ["player_1","knight"]
-        @board[0][2] = ["player_1","bishop"]
-        @board[0][3] = ["player_1","queen"]
-        @board[0][4] = ["player_1","king"]
-        @board[0][5] = ["player_1","bishop"]
-        @board[0][6] = ["player_1","knight"]
-        @board[0][7] = ["player_1","rook"]
-        @board[1].collect! { |c| c = ["player_1","pawn"] }
-        @board[6].collect! { |c| c = ["player_2","pawn"] }
-        @board[7][0] = ["player_2","rook"]
-        @board[7][1] = ["player_2","knight"]
-        @board[7][2] = ["player_2","bishop"]
-        @board[7][3] = ["player_2","queen"]
-        @board[7][4] = ["player_2","king"]
-        @board[7][5] = ["player_2","bishop"]
-        @board[7][6] = ["player_2","knight"]
-        @board[7][7] = ["player_2","rook"]
-        @player_turn = rand(2) == 1 ? "player_2" : "player_1"
-        @game_status = true
-        @last_casualty = nil
-        @input = nil 
+        if Dir.empty?("savegames")
+            puts "Couldn't found any saved game. Starting new game..."
+            @board = Array.new(8) { |r| Array.new(8) { |c| "" } }
+            @board[0][0] = ["player_1","rook"]
+            @board[0][1] = ["player_1","knight"]
+            @board[0][2] = ["player_1","bishop"]
+            @board[0][3] = ["player_1","queen"]
+            @board[0][4] = ["player_1","king"]
+            @board[0][5] = ["player_1","bishop"]
+            @board[0][6] = ["player_1","knight"]
+            @board[0][7] = ["player_1","rook"]
+            @board[1].collect! { |c| c = ["player_1","pawn"] }
+            @board[6].collect! { |c| c = ["player_2","pawn"] }
+            @board[7][0] = ["player_2","rook"]
+            @board[7][1] = ["player_2","knight"]
+            @board[7][2] = ["player_2","bishop"]
+            @board[7][3] = ["player_2","queen"]
+            @board[7][4] = ["player_2","king"]
+            @board[7][5] = ["player_2","bishop"]
+            @board[7][6] = ["player_2","knight"]
+            @board[7][7] = ["player_2","rook"]
+            @player_turn = rand(2) == 1 ? "player_2" : "player_1"
+            @game_status = true
+            @last_casualty = nil
+            @input = nil 
+            @castling_available_for_player_1 = true
+            @castling_available_for_player_2 = true
+        else
+            puts "Would you like to start a new game or load saved game? (N/L)"
+            decision = ""
+            while decision != "N" && decision != "L"
+                decision = gets.chomp.upcase
+                puts "Error: Input 'N' for new game or 'L' load game." if decision != "N" && decision != "L"
+            end
+            if decision == "N"
+                @board = Array.new(8) { |r| Array.new(8) { |c| "" } }
+                @board[0][0] = ["player_1","rook"]
+                @board[0][1] = ["player_1","knight"]
+                @board[0][2] = ["player_1","bishop"]
+                @board[0][3] = ["player_1","queen"]
+                @board[0][4] = ["player_1","king"]
+                @board[0][5] = ["player_1","bishop"]
+                @board[0][6] = ["player_1","knight"]
+                @board[0][7] = ["player_1","rook"]
+                @board[1].collect! { |c| c = ["player_1","pawn"] }
+                @board[6].collect! { |c| c = ["player_2","pawn"] }
+                @board[7][0] = ["player_2","rook"]
+                @board[7][1] = ["player_2","knight"]
+                @board[7][2] = ["player_2","bishop"]
+                @board[7][3] = ["player_2","queen"]
+                @board[7][4] = ["player_2","king"]
+                @board[7][5] = ["player_2","bishop"]
+                @board[7][6] = ["player_2","knight"]
+                @board[7][7] = ["player_2","rook"]
+                @player_turn = rand(2) == 1 ? "player_2" : "player_1"
+                @game_status = true
+                @last_casualty = nil
+                @input = nil 
+                @castling_available_for_player_1 = true
+                @castling_available_for_player_2 = true
+            elsif decision == "L"
+                puts "Saved games listed below."
+                list_num = 1
+                Dir.each_child("savegames") do |file| 
+                    puts "#{list_num}. #{file}"
+                end 
+                file_num = 0
+                while file_num < 1 && file_num > list_num
+                    puts "Input number next to file name for load game."
+                    file_num = gets.chomp.to_i
+                end
+                file_to_load = Dir.childeren("savegames")[file_num-1]
+                puts "Loading #{file_num}. #{file_to_load}..."
+                @game_status = true
+                @last_casualty = nil
+                @input = nil
+                row_i=0
+                col_i=0
+                load_num = 0
+                File.readlines(file_to_load) do |line|
+                    if load_num == 0
+                        @player_turn = line
+                    elsif load_num == 1
+                        @castling_available_for_player_1 = line
+                    elsif load_num == 2
+                        @castling_available_for_player_2 = line
+                    else
+                        if line == ""
+                            @board[row_i][col_i] = ""
+                        else
+                            loaded_piece = line.split(",")
+                            @board[row_i][col_i] = loaded_piece
+                        end
+                        col_i += 1
+                            if col_i > 7
+                                col_i = 0
+                                row_i += 1
+                                if row_i > 7
+                                    row_i = 0
+                                end
+                            end
+                    end
+                    load_num += 1
+                end
+            end
+        end
     end
 
     def print_board
+        puts "  0   1   2   3   4   5   6   7"
+        row_num = 0
         @board.each do |r|
-            puts "--------------------------------"
+            puts "---------------------------------"
             print "|"
             r.each do |c|
                 if c == ""
@@ -66,9 +151,11 @@ class Chess
                 end
                 print "|"
             end
+            print " #{row_num}"
             puts ""
+            row_num += 1
         end
-        puts "--------------------------------"
+        puts "---------------------------------"
     end
 
     def play 
@@ -93,7 +180,17 @@ class Chess
                 while move_piece(selected,target) == "invalid move"
                     puts "Invalid move. Please try again."
                     selected = select_piece_menu("select")
+                    if @input == "exit"
+                        @game_status = false
+                        puts "Exiting game..."
+                        return
+                    end
                     target = select_piece_menu("target")
+                    if @input == "exit"
+                        @game_status = false
+                        puts "Exiting game..."
+                        return
+                    end
                 end
                 @game_status = false if won? == true
                 @player_turn = "player_1"
@@ -102,9 +199,28 @@ class Chess
     end
 
     def save_game
-    end
-
-    def load_game
+        file_name = Date.new
+        line_num = 0
+        File.open(file_name,"w") do |line|
+            if line_num == 0
+                line = @player_turn
+            elsif line_num == 1
+                line = @castling_available_for_player_1
+            elsif line_num == 2
+                line = @castling_available_for_player_2
+            else
+                @board.each do |r|
+                    r.each do |c|
+                        if c == ""
+                            line = ""
+                        else
+                            line = "#{c[0]},#{c[1]}"
+                        end
+                    end
+                end
+            end
+            line_num += 1
+        end 
     end
 
     def reset_and_save
@@ -115,12 +231,16 @@ class Chess
         col = nil
         while row == nil || row < 0 || row > 7
             puts "Row:"
-            row = Integer(gets.chomp)
+            @input = gets.chomp
+            return if @input == "exit"
+            row = @input.to_i
             puts "Invalid input. Please try again." if row == nil || row < 0 || row > 7
         end
         while col == nil || col < 0 || col > 7
             puts "Column:"
-            col = Integer(gets.chomp)
+            @input = gets.chomp
+            return if @input == "exit"
+            col = @input.to_i
             puts "Invalid input. Please try again." if col == nil || col < 0 || col > 7
         end
         if opt == "select"
